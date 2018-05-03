e2a040ce49a77e6ac33b621ccf92c477689c4924
sijin <sijinkeeleri@gmial.com>
2018-05-03 11:25:19 +0530

    add Master page

---
+++ master.py
@@ -0,0 +1,17 @@
+from employee import Employee
+
+class Master(Employee):
+    emp = Employee()
+
+    def features(self):
+        name = input("Name :")
+        email = input("E-mail :")
+        n = input("Number of actions :")
+        actions = []
+        for i in n:
+            i = input("{} :".format(i))
+            actions.push(i)
+        self.emp.name = input("Employee Name :")
+        self.emp.salary = input("Employee Salary :")
+        print("Employee Name : {}, Employee Salary : {} ".format(self.emp.name, self.emp.salary))
+        
