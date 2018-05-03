5d4c2f985336e9741b0d388d4a2ec672ad8d9e1f
sijin <sijinkeeleri@gmial.com>
2018-05-03 12:01:21 +0530

    updates applied

--- employee.py
+++ employee.py
@@ -1,8 +1,24 @@
 
 class Employee():
- 
-    name = input("Name :")
-    email = input("Email :")
-    salary = input("Salary :")
-    print(" master : {}".format(self.master.name))
+    'Common base class for all employees'
+    empCount = 0
+
+    def __init__(self, eid, name, salary, did):
+        self.eid = eid
+        self.name = name
+        self.salary = salary
+        self.did = did
+        Employee.empCount += 1
+     
+    def displayEmployee(self):
+        print ("eid : ", self.eid,", Name : ", self.name,  ", Salary: ", self.salary,  ", did: ", self.did)
+    
+
+"This would create first object of Employee class"
+emp1 = Employee(1,"Zara", 2000,10)
+"This would create second object of Employee class"
+emp2 = Employee(2,"meera", 4000,20)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print ("Total Employee %d" % Employee.empCount)
 
