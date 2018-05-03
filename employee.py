3f4d2ca07176a34680377f34232017a813bf0c34
sijin <sijinkeeleri@gmial.com>
2018-05-03 11:44:26 +0530

    add Employee page

--- employee.py
+++ employee.py
@@ -1,9 +1,8 @@
-from master import Master
+
 class Employee():
-    master = Master() 
-    def features(self):
-        name = input("Name :")
-        email = input("Email :")
-        salary = input("Salary :")
-        print(" master : {}".format(self.master.name))
-        
+ 
+    name = input("Name :")
+    email = input("Email :")
+    salary = input("Salary :")
+    print(" master : {}".format(self.master.name))
+
