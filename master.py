c0bfbeaaa760d8ad1da9d5e2026173d006accf75
sijin <sijinkeeleri@gmial.com>
2018-05-03 12:59:17 +0530

    update 3

--- master.py
+++ master.py
@@ -1,17 +1,11 @@
 from employee import Employee
 
 class Master():
-    emp = Employee()
-
-    def features(self):
-        name = raw_input("Name :")
-        email = input("E-mail :")
-        n = input("Number of actions :")
-        actions = []
-        for i in n:
-            i = input("{} :".format(i))
-            actions.push(i)
-        self.emp.name = input("Employee Name :")
-        self.emp.salary = input("Employee Salary :")
-        print("Employee Name : {}, Employee Salary : {} ".format(self.emp.name, self.emp.salary))
-
+    n = input("number of employees :")
+    for i in range(0, n):
+        eid = input(" employee id : ")
+        name = raw_input("Name : ")
+        salary = input(" Salary : ")
+        did = input("did : ")
+        emp.push(Employee(eid, name, salary, did))
+        emp.displayEmployee()
