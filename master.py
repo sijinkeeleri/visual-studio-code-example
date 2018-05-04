e73157ea9fe78750d720267896783df17fed2f1c
sijin <sijinkeeleri@gmial.com>
2018-05-04 08:28:41 +0530

    update5  Master page

--- master.py
+++ master.py
@@ -1,11 +1,17 @@
 from employee import Employee
 
 class Master():
-    n = input("number of employees :")
-    for i in range(0, n):
-        eid = input(" employee id : ")
-        name = raw_input("Name : ")
-        salary = input(" Salary : ")
-        did = input("did : ")
-        emp = Employee(eid, name, salary, did)
-        emp.displayEmployee()
+    def addemployee():
+        n = input("number of employees :")
+        
+        for i in range(0, n):
+            eid = input(" employee id : ")
+            name = raw_input("Name : ")
+            salary = input(" Salary : ")
+            did = input("did : ")
+            emp = Employee(eid, name, salary, did)
+            emp.displayEmployee()
+
+    addemployee()
+
+
